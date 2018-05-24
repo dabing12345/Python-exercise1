ab9ca2b8fd17bf297228112e5d8d6f47dbf026c5
dabing12345 <30966697+dabing12345@users.noreply.github.com>
2018-05-24 15:40:31 +0800

    Add files via upload

---
+++ settings.py
@@ -0,0 +1,8 @@
+class Settings():
+    #存储游戏中所有设置的类
+    def __init__(self):
+        """初始化游戏的设置"""
+        #屏幕设置
+        self.creen_width = 1200
+        self.creen_height = 800
+        self.color = (230,230,230)
